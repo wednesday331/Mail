2b84a0498dea838a05baa2810083ed4ee9d3d1eb
wednesday331 <65116875+wednesday331@users.noreply.github.com>
2020-09-30 16:39:27 -0400

    PEP8 friendly formatting

--- mail/models.py
+++ mail/models.py
@@ -1,18 +1,29 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file contains all the relevant models used in views.py"""
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# User Class
 class User(AbstractUser):
+    """Abstract user model to be used for the different user accounts."""
     pass
 
-
+# Email Class
 class Email(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
-    sender = models.ForeignKey("User", on_delete=models.PROTECT, related_name="emails_sent")
-    recipients = models.ManyToManyField("User", related_name="emails_received")
+    """Contains the different elements involved in the email."""
+    user = models.ForeignKey("User",
+                             on_delete=models.CASCADE,
+                             related_name="emails")
+    sender = models.ForeignKey("User",
+                               on_delete=models.PROTECT,
+                               related_name="emails_sent")
+    recipients = models.ManyToManyField("User",
+                                        related_name="emails_received")
     subject = models.CharField(max_length=255)
     body = models.TextField(blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    time_stamp = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
     archived = models.BooleanField(default=False)
 
